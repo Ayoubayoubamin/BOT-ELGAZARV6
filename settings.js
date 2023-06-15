4d0070c637e38ad5527b5fd58e357457b80ffc8b
Ayoubayoubamin <136199247+Ayoubayoubamin@users.noreply.github.com>
2023-06-15 19:38:37 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,22 +16,22 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.ownername = "AYOUB INSANE❤️" //ur owner name
+global.ytname = "YT: AYOUB" //ur yt chanel name
+global.socialm = "GitHub:INSANE" //ur github or insta name
+global.location = "Africa, MAROC, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "0694213730" //ur number
+global.premium = ['0694213730'] //ur premium number
+global.botname = '❤️AYOUB BOT❤️' //ur bot name
+global.linkz = " https://www.youtube.com/@AYOUB-by3ml" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://www.youtube.com/@AYOUB-by3ml' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "ايوب الزعيم يروحي⍣" //ur sticker watermark packname
+global.author = "⍣0694213730⍣" //ur sticker watermark author
+global.wm = "❤️AYOUB❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
